9f7880d99e044e1d3376a796f14481732b38b7ac
Ayush <ayush.kanduri@gmail.com>
2022-09-11 11:38:31 +0530

    Setting up PassportJS & Passport Local Strategy

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,74 @@
+//Require the Passport Library
+const passport = require("passport");
+//Require the Passport Local Strategy
+const LocalStrategy = require("passport-local").Strategy;
+//Require the User Model
+const Employee = require("../models/employee");
+//Require the Bcrypt Library
+const bcrypt = require("bcryptjs");
+
+//--Authentication using PassportJS--//
+//PassportJS will use the Local Strategy function() to find the Employee who has signed in
+passport.use(
+	new LocalStrategy(
+		{
+			usernameField: "email", //The one in the DB
+			passReqToCallback: true,
+		},
+		async (req, email, password, done) => {
+			try {
+				const employee = await Employee.findOne({ email: email });
+				const isMatch = await bcrypt.compare(password, employee.password);
+				if (!employee || !isMatch) {
+					req.flash("error", "Invalid Username/Password ❌");
+					return done(null, false);
+				}
+				return done(null, employee);
+			} catch (err) {
+				console.log("Error in finding the Employee --> Passport ❌");
+				req.flash("error", err);
+				return done(err);
+			}
+		}
+	)
+);
+
+//--Serializing the Employee to decide which key is to be kept in the cookies--//
+passport.serializeUser((employee, done) => {
+	//It automatically encrypts the Employee ID into the cookie, & sends it to the browser
+	done(null, employee.id);
+});
+
+//--Deserializing the User from the key in the cookies--//
+passport.deserializeUser((id, done) => {
+	//It automatically decrypts the Employee ID from the cookie, & finds the Employee
+	Employee.findById(id, (err, employee) => {
+		if (err) {
+			console.log("Error in finding the Employee --> Passport ❌");
+			return done(err);
+		}
+		return done(null, employee);
+	});
+});
+
+//Creating a middleware in passport.js
+passport.checkAuthentication = function (req, res, next) {
+	//If the Employee is signed in then let them have the profile page
+	if (req.isAuthenticated()) return next();
+	//If the Employee is not signed in, then redirect them back to the Login page
+	return res.redirect("/");
+};
+
+//Creating a middleware in passport.js
+passport.setAuthenticatedUser = function (req, res, next) {
+	//Whenever a Employee is signed in, then that Employee's info is available in req.user
+	if (req.isAuthenticated()) res.locals.user = req.user;
+	//req.user contains the current signed in user from the session cookie
+	next();
+};
+
+//Export the Passport Module
+module.exports = passport;
+
+// const salt = await bcrypt.genSalt(10);
+// const hashedPassword = await bcrypt.hash(password, salt);
--- index.js
+++ index.js
@@ -20,6 +20,14 @@ const ejs = require("ejs");
 const sassMiddleware = require("node-sass-middleware");
 //Require the Morgan Module for Logging
 const logger = require("morgan");
+//Requires the Cookie-Parser Module
+const cookieParser = require("cookie-parser");
+//Requires the Express-Session Module for the Session Cookie
+const session = require("express-session");
+//Requires the MongoStore
+const MongoStore = require("connect-mongo");
+//Requires the PassportJS Module for the Authentication
+const passport = require("passport");
 //Require the View Helpers
 const viewHelpers = require("./config/view-helpers")(app);
 //Require the Environment File for getting the Environment Variables
@@ -30,6 +38,8 @@ const db = require("./config/mongoose");
 const route = require("./routes/index");
 //Requires the Custom Middleware
 const customMiddleware = require("./config/middleware");
+//Requires the Passport Local Strategy used for the Authentication
+const passportLocal = require("./config/passport-local-strategy");
 
 //Middleware - CORS
 app.use(cors());
@@ -54,8 +64,14 @@ if (env.name == "development") {
 app.use(express.urlencoded({ extended: true }));
 //Middleware - JSON Encoder
 app.use(express.json());
+//Middleware - Cookie Parser for accessing & parsing the cookies
+app.use(cookieParser());
 //Middleware - Express App uses Static Files in the Assets Folder
 app.use(express.static(env.asset_path));
+//Middleware - Make the '/storage' path available to the browser
+app.use("/storage", express.static(__dirname + "/storage"));
+//Middleware - Make the '/uploads' path available to the browser
+app.use("/uploads", express.static(__dirname + "/uploads"));
 //Middleware - Express App uses expressLayouts to tell that the views which are going to be rendered belongs to some layout.
 app.use(expressLayouts);
 //Middleware - Morgan used for Logging
@@ -70,6 +86,47 @@ app.set("view engine", "ejs");
 //Set Up - Template Engine Views Folder Path (..../views)
 app.set("views", path.join(__dirname, "views"));
 
+//Middleware - Creates a Session Cookie and Encrypts it with a Key
+app.use(
+	session({
+		//Cookie Name
+		name: "PlacementCellApplication",
+		//Secret Key for encrypting the session cookie
+		secret: env.session_cookie_key,
+		//Don't save the uninitialized session
+		saveUninitialized: false,
+		//Don't re-save the session if it is not modified
+		resave: false,
+		//Cookie Options
+		cookie: {
+			//Cookie Expiry Time - 100 Minutes
+			maxAge: 1000 * 60 * 100,
+		},
+		//MongoStore is used to store the Session Cookies in the MongoDB
+		store: MongoStore.create(
+			{
+				//DB Connection URL
+				mongoUrl: `${env.db}`,
+				//Interacts with the mongoose to connect to the MongoDB
+				mongooseConnection: db,
+				//To auto remove the store
+				autoRemove: "disabled",
+			},
+			(err) => {
+				//If there is an error
+				if (err) {
+					console.log(err || "connect-mongodb setup ok");
+				}
+			}
+		),
+	})
+);
+//Middleware - Initializes & uses PassportJS for the Authentication
+app.use(passport.initialize());
+//Middleware - PassportJS creates & maintains the Session
+app.use(passport.session());
+//Middleware - Sets the Authenticated User in the Response
+app.use(passport.setAuthenticatedUser);
 //Middleware - Creates Folders if they don't exist
 app.use(customMiddleware.createFolders);
 //Middleware - App calls index.js - Route File, whenever '/' route is called in the request.
